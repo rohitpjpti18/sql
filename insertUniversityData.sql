5d50ec6a20c1db212aeda52d77a9ec25412bb70e
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-05-31 12:39:11 +0530

    update: added rows for all tables

--- insertUniversityData.sql
+++ insertUniversityData.sql
@@ -1,3 +1,29 @@
+-- Insert rows into table 'time_slot'
+INSERT INTO timeslot
+(time_slot_id, day, start_time, end_time)
+VALUES
+('A', 'M', '08:00:00', '08:50:00'),
+('B', 'W', '08:00:00', '08:50:00'),
+('C', 'F', '08:00:00', '08:50:00'),
+('D', 'M', '09:00:00', '09:50:00'),
+('E', 'W', '09:00:00', '09:50:00'),
+('F', 'F', '09:00:00', '09:50:00'),
+('G', 'M', '11:00:00', '11:50:00'),
+('H', 'W', '11:00:00', '11:50:00'),
+('I', 'F', '11:00:00', '11:50:00'),
+('J', 'M', '13:00:00', '13:50:00'),
+('L', 'W', '13:00:00', '13:50:00'),
+('M', 'F', '13:00:00', '13:50:00'),
+('N', 'T', '10:30:00', '11:45:00'),
+('O', 'R', '10:30:00', '11:45:00'),
+('P', 'T', '14:30:00', '15:45:00'),
+('Q', 'R', '14:30:00', '15:45:00'),
+('R', 'M', '16:00:00', '16:50:00'),
+('S', 'W', '16:00:00', '16:50:00'),
+('T', 'F', '16:00:00', '16:50:00'),
+('U', 'W', '10:00:00', '12:30:00');
+
+
 -- Insert rows into table 'classroom'
 INSERT INTO classroom
 (building, room_number, capacity)
@@ -21,6 +47,41 @@ VALUES
 ('Physics', 'Watson', 70000);
 
 -- Insert rows into table 'course'
+INSERT INTO instructor
+(ID, name, dept_name, salary)
+VALUES
+('10101', 'Srinivasan', 'Comp. Sci.', 65000),
+('12121', 'Wu', 'Finance', 90000),
+('15151', 'Mozart', 'Music', 40000),
+('22222', 'Einstein', 'Physics', 95000),
+('32343', 'El Said', 'History', 60000),
+('33456', 'Gold', 'Physics', 87000),
+('45565', 'Katz', 'Comp. Sci.', 75000),
+('58583', 'Califieri', 'History', 62000),
+('76543', 'Singh', 'Finance', 80000),
+('76766', 'Crick', 'Biology', 72000),
+('83821', 'Brandt', 'Comp. Sci.', 92000),
+('98345', 'Kim', 'Elec. Eng.', 80000);
+
+-- Insert rows into table 'student'
+INSERT INTO student
+(ID, name, dept_name, tot_cred)
+VALUES
+('00128', 'Zhang', 'Comp. Sci.', 102),
+('12345', 'Shanker', 'Comp. Sci.', 32),
+('19991', 'Brandt', 'History', 80),
+('23121', 'Chavez', 'Finance', 110),
+('44553', 'Peltier', 'Physics', 56),
+('45678', 'Levy', 'Physics', 46),
+('54321', 'Williams', 'Comp. Sci.', 54),
+('55739', 'Sanchez', 'Music', 38),
+('70557', 'Snow', 'Physics', 0),
+('76543', 'Brown', 'Comp. Sci.', 58),
+('76653', 'Aoi', 'Elec. Eng.', 60),
+('98765', 'Bourikas', 'Elec. Eng.', 98),
+('98988', 'Tanaka', 'Biology', 120);
+
+-- Insert rows into table 'course'
 INSERT INTO course
 (course_id, title, dept_name, credits)
 VALUES
@@ -39,18 +100,102 @@ VALUES
 ('PHY-101', 'Physical Principles', 'Physics', 4);
 
 
+
 -- Insert rows into table 'course'
-INSERT INTO instructor
-(ID, name, dept_name, salary)
+INSERT INTO section
+(course_id, sec_id, semester, year, building, room_number, time_slot_id)
 VALUES
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(12121, 'Wu', 'Finance', 90000),
-(10101, 'Srinivasan', 'Comp. Sci.', 650),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000),
-(10101, 'Srinivasan', 'Comp. Sci.', 65000);
+('BIO-101', 1, 'Summer', 2009, 'Painter', '514', 'B'),
+('BIO-301', 1, 'Summer', 2010, 'Painter', '514', 'A'),
+('CS-101', 1, 'Fall', 2009, 'Packard', '101', 'H'),
+('CS-101', 1, 'Spring', 2010, 'Packard', '101', 'F'),
+('CS-190', 1, 'Spring', 2009, 'Taylor', '3128', 'E'),
+('CS-190', 2, 'Spring', 2009, 'Taylor', '3128', 'A'),
+('CS-315', 1, 'Spring', 2010, 'Watson', '120', 'D'),
+('CS-319', 1, 'Spring', 2010, 'Watson', '100', 'B'),
+('CS-319', 2, 'Spring', 2010, 'Taylor', '3128', 'C'),
+('CS-347', 1, 'Fall', 2009, 'Taylor', '3128', 'A'),
+('EE-181', 1, 'Spring', 2009, 'Taylor', '3128', 'C'),
+('FIN-201', 1, 'Spring', 2010, 'Packard', '101', 'B'),
+('HIS-351', 1, 'Spring', 2010, 'Painter', '514', 'C'),
+('MU-199', 1, 'Spring', 2010, 'Packard', '101', 'D'),
+('PHY-101', 1, 'Fall', 2009, 'Watson', '100', 'A');
+
+-- Insert rows into table 'teaches'
+INSERT INTO teaches
+(ID, course_id, sec_id, semester, year)
+VALUES
+('10101', 'CS-101', 1, 'Fall', 2009),
+('10101', 'CS-315', 1, 'Spring', 2010),
+('10101', 'CS-347', 1, 'Fall', 2009),
+('12121', 'FIN-201', 1, 'Spring', 2010),
+('15151', 'MU-199', 1, 'Spring', 2010),
+('22222', 'PHY-101', 1, 'Fall', 2009),
+('32343', 'HIS-351', 1, 'Spring', 2010),
+('45565', 'CS-101', 1, 'Spring', 2010),
+('45565', 'CS-319', 1, 'Spring', 2010),
+('76766', 'BIO-101', 1, 'Summer', 2009),
+('76766', 'BIO-301', 1, 'Summer', 2010),
+('83821', 'CS-190', 1, 'Spring', 2009),
+('83821', 'CS-190', 2, 'Spring', 2009),
+('83821', 'CS-319', 2, 'Spring', 2010),
+('98345', 'CS-190', 1, 'Spring', 2009);
+
+
+
+-- Insert rows into table 'takes'
+INSERT INTO takes
+(ID, course_id, sec_id, semester, year, grade)
+VALUES
+('00128', 'CS-101', 1, 'Fall', 2009, 'A'),
+('00128', 'CS-347', 1, 'Fall', 2009, 'A-'),
+('12345', 'CS-101', 1, 'Fall', 2009, 'C'),
+('12345', 'CS-190', 2, 'Spring', 2009, 'A'),
+('12345', 'CS-315', 1, 'Spring', 2010, 'A'),
+('12345', 'CS-347', 1, 'Fall', 2009, 'A'),
+('19991', 'HIS-351', 1, 'Spring', 2010, 'B'),
+('23121', 'FIN-201', 1, 'Spring', 2010, 'C+'),
+('44553', 'PHY-101', 1, 'Fall', 2009, 'B-'),
+('45678', 'CS-101', 1, 'Fall', 2009, 'F'),
+('45678', 'CS-101', 1, 'Spring', 2010, 'B+'),
+('45678', 'CS-319', 1, 'Spring', 2010, 'B'),
+('54321', 'CS-101', 1, 'Fall', 2009, 'A-'),
+('54321', 'CS-190', 2, 'Spring', 2009, 'B+'),
+('55739', 'MU-199', 1, 'Spring', 2010, 'A-'),
+('76543', 'CS-101', 1, 'Fall', 2009, 'A'),
+('76543', 'CS-319', 2, 'Spring', 2010, 'A'),
+('76653', 'EE-181', 1, 'Spring', 2009, 'C'),
+('98765', 'CS-101', 1, 'Fall', 2009, 'C-'),
+('98765', 'CS-315', 1, 'Spring', 2010, 'B'),
+('98988', 'BIO-101', 1, 'Summer', 2009, 'A'),
+('98988', 'BIO-301', 1, 'Summer', 2010, NULL);
+
+-- Insert rows into table 'advisor'
+INSERT INTO advisor
+(s_id, i_id)
+VALUES
+('00128','45565'),
+('12345','10101'),
+('23121','76543'),
+('44553','22222'),
+('45678','22222'),
+('76543','45565'),
+('76653','98345'),
+('98765','98345'),
+('98988','76766');
+
+-- Insert rows into table 'prereq'
+INSERT INTO prereq
+(course_id, prereq_id)
+VALUES
+('BIO-301', 'BIO-101'),
+('BIO-399', 'BIO-101'),
+('CS-190', 'CS-101'),
+('CS-315', 'CS-101'),
+('CS-319', 'CS-101'),
+('CS-347', 'CS-101'),
+('EE-181', 'PHY-101');
+
+
+
+
